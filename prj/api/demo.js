e2d254f0f269f3af742b65f55442e8f400868822
Isaac Gorman <emailme@isaacg.net>
2020-11-04 12:19:17 -0600

    first gq server

---
+++ prj/api/demo.js
@@ -0,0 +1,36 @@
+const { ApolloServer } = require('apollo-server')
+const gql = require('graphql-tag')
+
+const typeDefs = gql`
+    type User {
+        email: String!
+        avatar: String!
+        friends: [User]!
+    }
+
+    type Query {
+        me: User!
+    }
+`
+
+const resolvers ={
+    Query: {
+        me(){
+         return{
+            email: "yoda@me.com",
+            avatar: "yoda.png",
+            friends: []
+         }
+        }
+    }
+}
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+
+server.listen(4000)
+    .then(()=> console.log('on port 4000'))
